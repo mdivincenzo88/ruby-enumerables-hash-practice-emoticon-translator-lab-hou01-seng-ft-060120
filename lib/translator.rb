4abe8ceceaf35e48bc6304e7ad18ca28e413cf37
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-25 19:52:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_english_meaning (file_path,emoticon)
   library = load_library (file_path)
  
 library.each do |key, value|
-  if value[:english] ==emoticon
+  if emoticon == value[:english]
     return value[:japanese]
  
      end
